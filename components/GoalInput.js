a645618434b86149c78edf08cf479fa0af436d9c
Arash <arash8877@gmail.com>
2024-03-13 20:46:36 +0100

    modified modal

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -20,11 +20,18 @@ const GoalInput = ({ setCourseGoals, showModal }) => {
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
-          placeholder="Your course goal"
+          placeholder="Your course goal..."
           onChangeText={goalInputHandler}
           value={enteredGoalText}
         />
-        <Button title="Add Goal" onPress={addGoalHandler} />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" />
+          </View>
+        </View>
       </View>
     </Modal>
   );
@@ -35,9 +42,9 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
+    padding: 16,
     marginBottom: 24,
     borderBottomWidth: 1,
     borderColor: "#cccccc",
@@ -46,8 +53,15 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#cccccc",
     borderRadius: 5,
-    width: "70%",
-    paddingLeft: 15,
-    marginRight: 8,
+    width: "100%",
+    padding: 10,
   },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
+  }
 });
